3cb1e650708592bca94691283c391441dc4e4efa
ZestyCardboard <59447156+ZestyCardboard@users.noreply.github.com>
2020-12-10 22:04:27 -0500

    added a flame particle

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    
     public float speed = 5f;
     private float movementHorizontal = 0f;
     private float movementVertical = 0f;
